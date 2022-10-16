4b05cdca390d8f8a51613dbc5d5bd12972e2dbff
Ilya Khegay <i.v.khegay@gmail.com>
2022-10-17 00:14:16 +0700

    ButtonCounter

---
+++ base/src/jsMain/kotlin/entity/PluralText.kt
@@ -0,0 +1,10 @@
+package entity
+
+fun interface PluralText {
+    fun pluralize(count: Int): String
+}
+
+data class EnglishPlural(val singular: String, val plural: String) : PluralText {
+    override fun pluralize(count: Int): String =
+        if (count == 1) singular else plural
+}
---
+++ base/src/jsMain/kotlin/ui/common/basecomponent/ButtonCounter.kt
@@ -0,0 +1,35 @@
+package ui.common.basecomponent
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.size
+import com.varabyte.kobweb.silk.components.icons.fa.FaMinus
+import com.varabyte.kobweb.silk.components.icons.fa.FaPlus
+import com.varabyte.kobweb.silk.components.text.SpanText
+import entity.PluralText
+import org.jetbrains.compose.web.css.em
+
+@Composable
+fun ButtonCounter(count: Int, suffix: PluralText? = null, modifier: Modifier = Modifier, onCountChange: (Int) -> Unit) {
+
+    Row(modifier, verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+        RowButton(
+            onClick = { onCountChange(count - 1) },
+            modifier = Modifier.size(1.2.em)
+        ) { FaMinus() }
+
+        if (suffix == null) {
+            SpanText(count.toString())
+        } else {
+            SpanText(("$count ${suffix.pluralize(count)}"))
+        }
+
+        RowButton(
+            onClick = { onCountChange(count + 1) },
+            modifier = Modifier.size(1.2.em)
+        ) { FaPlus() }
+    }
+}
