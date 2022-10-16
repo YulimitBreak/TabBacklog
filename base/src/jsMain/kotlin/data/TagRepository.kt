34ff1620ca916a584250b538879c0f7b4a081522
Ilya Khegay <i.v.khegay@gmail.com>
2022-10-16 23:46:08 +0700

    Actual tag search

--- base/src/jsMain/kotlin/data/TagRepository.kt
+++ base/src/jsMain/kotlin/data/TagRepository.kt
@@ -1,13 +1,25 @@
 package data
 
-import kotlinx.coroutines.delay
+import com.juul.indexeddb.bound
+import data.database.core.DatabaseHolder
+import data.database.core.DbSchema
+import data.database.schema.TagSchema
 
-class TagRepository {
+class TagRepository(private val databaseHolder: DatabaseHolder) {
 
     suspend fun fetchTagAutocomplete(tag: String): List<String> {
         if (tag.isBlank()) return emptyList()
-        delay(1000)
-        return words.asSequence().filter { it.startsWith(tag.trim()) }.sorted().take(6).toList()
+        val schema = DbSchema<TagSchema>()
+        return databaseHolder.database().transaction(schema.storeName) {
+            val tags = objectStore(schema.storeName).index(TagSchema.Tag.name)
+                .getAll(bound(tag, tag + '\uffff'))
+                .map { schema.extract(it) { TagSchema.Tag.value<String>() } }
+            tags.groupBy { it }
+                .mapValues { it.value.size }.toList()
+                .sortedByDescending { it.second }
+                .take(6)
+                .map { it.first }
+        }
     }
 
     companion object {
