7d6e14450e04d9aa9405d7d1ba17bcccb525ec31
Ilya Khegay <i.v.khegay@gmail.com>
2022-10-17 23:31:16 +0700

    Move slider with mouse

--- base/src/jsMain/kotlin/ui/common/basecomponent/Slider.kt
+++ base/src/jsMain/kotlin/ui/common/basecomponent/Slider.kt
@@ -1,17 +1,25 @@
 package ui.common.basecomponent
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.asAttributesBuilder
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.style.toModifier
+import com.varabyte.kobweb.silk.components.text.SpanText
+import common.coerceIn
+import kotlinx.browser.document
+import org.jetbrains.compose.web.attributes.Draggable
 import org.jetbrains.compose.web.css.minus
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Div
+import org.w3c.dom.DragEvent
+import org.w3c.dom.Image
+import org.w3c.dom.events.Event
 import ui.common.styles.components.SliderComponent
+import kotlin.math.roundToInt
 
 @Composable
 fun Slider(
@@ -22,16 +30,48 @@ fun Slider(
 ) {
 
     val percent = value * 100f / maxValue.coerceAtLeast(1)
-    Box(contentAlignment = Alignment.Center, modifier = modifier.role("slider")) {
+    var sliderWidth by remember { mutableStateOf(1) }
+    var sliderClientOffset by remember { mutableStateOf(0.0) }
+    var positionText by remember { mutableStateOf("") }
+
+    SpanText("client offset $sliderClientOffset")
+    SpanText(positionText)
+
+    fun updatePositionByLocation(clientX: Int) {
+        if (clientX == 0) return
+        if (sliderWidth == 0) return
+        val offsetX = clientX.toFloat() - sliderClientOffset
+        val newPosition = ((offsetX / sliderWidth) * maxValue).roundToInt().coerceIn(0, maxValue)
+        positionText = "offsetX = $offsetX / sliderWidth = $sliderWidth = newPosition = $newPosition"
+        if (newPosition != value) {
+            onValueChange(newPosition)
+        }
+    }
+
+    Box(contentAlignment = Alignment.Center, modifier = modifier.role("slider")
+        .onClick { event ->
+            console.log(event)
+            updatePositionByLocation(clientX = event.clientX)
+        }
+    ) {
         // Nesting containers to avoid a problem with paddings
         Box(
             contentAlignment = Alignment.Center,
+            elementScope = {
+                DisposableEffect(Unit) {
+                    sliderWidth = scopeElement.offsetWidth
+                    sliderClientOffset = scopeElement.getBoundingClientRect().left
+                    console.log(scopeElement)
+                    onDispose { }
+                }
+            },
             modifier = Modifier.fillMaxHeight().width(100.percent - 16.px).margin(leftRight = 8.px)
         ) {
             Div(
                 attrs = SliderComponent.TrackStyle.toModifier()
                     .width(percent.percent)
                     .align(Alignment.CenterStart)
+                    .transition("width 0.1s ease")
                     .asAttributesBuilder()
             )
 
@@ -39,6 +79,7 @@ fun Slider(
                 attrs = SliderComponent.InactiveTrackStyle.toModifier()
                     .width((100 - percent).percent)
                     .align(Alignment.CenterEnd)
+                    .transition("width 0.1s ease")
                     .asAttributesBuilder()
             )
 
@@ -47,6 +88,7 @@ fun Slider(
                     .tabIndex(0)
                     .align(Alignment.CenterStart)
                     .margin(left = percent.percent - 8.px)
+                    .transition("margin 0.1s ease")
                     .onKeyDown { event ->
                         if (event.getNormalizedKey() == "ArrowLeft") {
                             onValueChange((value - 1).coerceAtLeast(0))
@@ -54,8 +96,35 @@ fun Slider(
                             onValueChange((value + 1).coerceAtMost(maxValue))
                         }
                     }
-                    .asAttributesBuilder()
+                    .draggable(Draggable.True)
+                    .asAttributesBuilder {
+                        onDragStart { event ->
+                            // https://stackoverflow.com/a/49535378
+                            val image = Image()
+                            image.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
+                            event.dataTransfer?.setDragImage(image, 0, 0)
+                            event.dataTransfer?.clearData()
+                            event.dataTransfer?.setData(DRAG_EVENT_DATA_TYPE, "")
+                        }
+                        onDrag { event ->
+                            updatePositionByLocation(event.clientX)
+                        }
+                    }
             )
         }
     }
+
+    DisposableEffect(Unit) {
+        val callback: (Event) -> Unit = { event ->
+            if (event is DragEvent && event.dataTransfer?.types.orEmpty().contains(DRAG_EVENT_DATA_TYPE)) {
+                event.preventDefault()
+            }
+        }
+        document.addEventListener("dragover", callback)
+        onDispose {
+            document.removeEventListener("dragover", callback)
+        }
+    }
 }
+
+private const val DRAG_EVENT_DATA_TYPE = "thumb_data_type"
