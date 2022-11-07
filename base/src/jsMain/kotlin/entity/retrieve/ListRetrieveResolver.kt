eb9bb19f9d154450910be6bc2898bf3dfe713ee0
Ilya Khegay <i.v.khegay@gmail.com>
2022-11-07 19:03:03 +0700

    Rework Join in resolver

--- base/src/jsMain/kotlin/entity/retrieve/ListRetrieveResolver.kt
+++ base/src/jsMain/kotlin/entity/retrieve/ListRetrieveResolver.kt
@@ -5,63 +5,6 @@ import kotlinx.coroutines.flow.asFlow
 
 abstract class ListRetrieveResolver<T, Query : RetrieveQuery<T>>(val source: List<T>) : RetrieveResolver<T, Query>() {
 
-    abstract fun handleSelect(source: List<T>, query: Query): List<T>
-
-    override fun resolveFetch(actions: List<RetrieveBuilder.Action<T, Query>>): Flow<T> {
-        var result = source
-        actions.forEach { action ->
-            result = when (action) {
-                is RetrieveBuilder.Action.Filter -> result.filter { action.criteria(it) }
-                is RetrieveBuilder.Action.Limit -> result.take(action.count)
-                is RetrieveBuilder.Action.ListAction -> action.transform(result)
-                is RetrieveBuilder.Action.Map -> result.map { action.transform(it) }
-                is RetrieveBuilder.Action.Select -> handleSelect(result, action.query)
-            }
-        }
-        return result.asFlow()
-    }
-
-
-    protected fun <R : Comparable<R>> sortByField(
-        source: List<T>,
-        field: (T) -> R?,
-        query: RetrieveQuery.Sort<T, R>
-    ) =
-        sortByField(
-            source, field, naturalOrder(), query.ascending, query.from, query.to, query.fallbackSort
-        )
-
-    protected fun <R : Any> sortByField(
-        source: List<T>,
-        field: (T) -> R?,
-        comparator: Comparator<R>,
-        query: RetrieveQuery.Sort<T, R>
-    ) =
-        sortByField(
-            source, field, comparator, query.ascending, query.from, query.to, query.fallbackSort
-        )
-
-    @Suppress("MemberVisibilityCanBePrivate")
-    protected fun <R> sortByField(
-        source: List<T>, field: (T) -> R?,
-        comparator: Comparator<R>,
-        ascending: Boolean,
-        from: R?,
-        to: R?,
-        fallbackSort: Comparator<T>?,
-    ): List<T> {
-        var result = source.filter { field(it) != null }
-            .sortedWith { a, b ->
-                val compareValues =
-                    (if (ascending) comparator else comparator.reversed()).compare(field(a)!!, field(b)!!)
-                if (compareValues == 0) fallbackSort?.compare(a, b) ?: compareValues else compareValues
-            }
-        if (from != null) {
-            result = result.dropWhile { comparator.compare(field(it)!!, from) < 0 }
-        }
-        if (to != null) {
-            result = result.dropLastWhile { comparator.compare(field(it)!!, to) > 0 }
-        }
-        return result
-    }
+    override fun resolveFetch(actions: List<RetrieveBuilder.Action<T, Query>>): Flow<T> =
+        applyToList(source, actions).asFlow()
 }
--- base/src/jsMain/kotlin/entity/sort/BookmarkListRetrieveResolver.kt
+++ base/src/jsMain/kotlin/entity/sort/BookmarkListRetrieveResolver.kt
@@ -6,15 +6,16 @@ import entity.retrieve.ListRetrieveResolver
 class BookmarkListRetrieveResolver(source: List<Bookmark>) :
     ListRetrieveResolver<Bookmark, BookmarkRetrieveQuery>(source) {
 
-    override fun handleSelect(source: List<Bookmark>, query: BookmarkRetrieveQuery): List<Bookmark> = when (query) {
-        is BookmarkRetrieveQuery.CreationDate -> sortByField(source, Bookmark::creationDate, query)
-        is BookmarkRetrieveQuery.Deadline -> sortByField(source, Bookmark::deadline, query)
-        is BookmarkRetrieveQuery.ExpirationDate -> sortByField(source, Bookmark::expirationDate, query)
-        is BookmarkRetrieveQuery.Favorite -> source.filter { it.favorite == query.target }
-        is BookmarkRetrieveQuery.RemindDate -> sortByField(source, Bookmark::remindDate, query)
-        is BookmarkRetrieveQuery.Title -> sortByField(source, Bookmark::title, query)
-        is BookmarkRetrieveQuery.Type -> source.filter { it.type == query.target }
-        is BookmarkRetrieveQuery.Url -> sortByField(source, Bookmark::url, query)
-    }
+    override fun applySelectToList(source: List<Bookmark>, query: BookmarkRetrieveQuery): List<Bookmark> =
+        when (query) {
+            is BookmarkRetrieveQuery.CreationDate -> sortListByField(source, Bookmark::creationDate, query)
+            is BookmarkRetrieveQuery.Deadline -> sortListByField(source, Bookmark::deadline, query)
+            is BookmarkRetrieveQuery.ExpirationDate -> sortListByField(source, Bookmark::expirationDate, query)
+            is BookmarkRetrieveQuery.Favorite -> source.filter { it.favorite == query.target }
+            is BookmarkRetrieveQuery.RemindDate -> sortListByField(source, Bookmark::remindDate, query)
+            is BookmarkRetrieveQuery.Title -> sortListByField(source, Bookmark::title, query)
+            is BookmarkRetrieveQuery.Type -> source.filter { it.type == query.target }
+            is BookmarkRetrieveQuery.Url -> sortListByField(source, Bookmark::url, query)
+        }
 
 }
