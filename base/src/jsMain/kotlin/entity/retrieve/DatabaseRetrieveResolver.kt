07a69a624e63dc981a8eba8d2af7a0b298c170f6
Ilya Khegay <i.v.khegay@gmail.com>
2022-11-15 02:55:46 +0700

    Basic DatabaseRetrieveResolver

---
+++ base/src/jsMain/kotlin/entity/retrieve/DatabaseRetrieveResolver.kt
@@ -0,0 +1,108 @@
+package entity.retrieve
+
+import com.juul.indexeddb.*
+import common.listTransform
+import data.database.core.paginate
+import entity.retrieve.util.selectByField
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+
+abstract class DatabaseRetrieveResolver<T, Query : RetrieveQuery<T>>(
+    val database: suspend () -> Database,
+) :
+    RetrieveResolver<T, Query>() {
+
+    protected abstract val storeName: String
+    protected open val paginationPageSize: Int = 10
+
+    protected abstract fun extract(source: dynamic): T
+
+    protected abstract suspend fun resolveQuery(query: Query): DatabaseQuery<T>
+
+    override suspend fun fetchFlow(query: Query?, hasReorderingActions: Boolean): Flow<T> =
+        // TODO handle fallbackSort
+        if (!hasReorderingActions) {
+            val dbQuery = query?.let { resolveQuery(it) }
+            database().paginate(
+                storeName, dbQuery?.indexName, dbQuery?.key,
+                if (dbQuery?.reverse == true) Cursor.Direction.Previous else Cursor.Direction.Next,
+                paginationPageSize
+            ) {
+                extract(it.value)
+            }
+        } else {
+            database().transaction(storeName) {
+                val dbQuery = query?.let { resolveQuery(it) }
+                val store = objectStore(storeName)
+                val queryable = dbQuery?.indexName?.let { store.index(dbQuery.indexName) } ?: store
+                queryable.getAll(dbQuery?.key)
+                    .map { extract(it) }
+                    .let { if (dbQuery?.reverse == true) it.asReversed() else it }
+            }.asFlow()
+        }.map {
+            postFetch(it)
+        }
+
+    protected open suspend fun postFetch(data: T): T = data
+
+    protected fun <R : Any> Flow<T>.applySelectToFlow(
+        field: (T) -> R?,
+        comparator: Comparator<R>,
+        query: RetrieveQuery.Sort<T, R>
+    ) =
+        this.listTransform { list ->
+            list.selectByField(field, comparator, query)
+        }
+
+    protected fun <R : Comparable<R>> Flow<T>.applySelectToFlow(
+        field: (T) -> R?,
+        query: RetrieveQuery.Sort<T, R>
+    ) =
+        this.listTransform { list ->
+            list.selectByField(field, query)
+        }
+
+    protected data class FallbackSort<T, R : Any>(val compareField: (T) -> R?, val fallbackSort: Comparator<T>)
+
+    protected data class DatabaseQuery<T>(
+        val indexName: String?,
+        val key: Key?,
+        val reverse: Boolean = false,
+        val fallback: FallbackSort<T, *>?
+    ) {
+        companion object {
+            operator fun <T, R : Any> invoke(
+                indexName: String,
+                field: ((T) -> R?)?,
+                sortQuery: RetrieveQuery.Sort<T, R>,
+                typeMapper: ((R) -> dynamic)? = null
+            ): DatabaseQuery<T> {
+                val from = sortQuery.from?.let { typeMapper?.invoke(it) ?: it }
+                val to = sortQuery.to?.let { typeMapper?.invoke(it) ?: it }
+                val key = when {
+                    from != null && to != null -> bound(from, to)
+                    from != null -> lowerBound(from)
+                    to != null -> upperBound(to)
+                    else -> null
+                }
+                val fallbackSort = sortQuery.fallbackSort
+                val fallback = if (fallbackSort != null && field != null) {
+                    FallbackSort(field, fallbackSort)
+                } else null
+                return DatabaseQuery(indexName, key, !sortQuery.ascending, fallback)
+            }
+
+            operator fun <T, R : Any> invoke(
+                indexName: String,
+                filterQuery: RetrieveQuery.Filter<T, R>,
+                typeMapper: ((R) -> dynamic)? = null
+            ) = DatabaseQuery<T>(
+                indexName,
+                Key(filterQuery.target.let { typeMapper?.invoke(it) ?: it }),
+                false,
+                null
+            )
+        }
+    }
+}
---
+++ base/src/jsMain/kotlin/entity/sort/BookmarkDatabaseRetrieveResolver.kt
@@ -0,0 +1,63 @@
+package entity.sort
+
+import com.juul.indexeddb.Database
+import data.database.core.DbSchema
+import data.database.schema.BookmarkSchema
+import data.database.schema.extractObject
+import entity.Bookmark
+import entity.retrieve.DatabaseRetrieveResolver
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
+
+class BookmarkDatabaseRetrieveResolver(database: suspend () -> Database) :
+    DatabaseRetrieveResolver<Bookmark, BookmarkRetrieveQuery>(database) {
+
+    private val schema = DbSchema<BookmarkSchema>()
+
+    override val storeName: String = schema.storeName
+
+    override fun extract(source: dynamic): Bookmark = schema.extractObject(source)
+
+    override suspend fun resolveQuery(query: BookmarkRetrieveQuery): DatabaseQuery<Bookmark> = when (query) {
+        is BookmarkRetrieveQuery.CreationDate ->
+            DatabaseQuery(BookmarkSchema.CreationDate.name, Bookmark::creationDate, query) { it.toString() }
+
+        is BookmarkRetrieveQuery.Deadline ->
+            DatabaseQuery(BookmarkSchema.Deadline.name, Bookmark::deadline, query) { it.toString() }
+
+        is BookmarkRetrieveQuery.ExpirationDate ->
+            DatabaseQuery(BookmarkSchema.ExpirationDate.name, Bookmark::expirationDate, query) { it.toString() }
+
+        is BookmarkRetrieveQuery.Favorite ->
+            DatabaseQuery(BookmarkSchema.Favorite.name, query)
+
+        is BookmarkRetrieveQuery.RemindDate ->
+            DatabaseQuery(BookmarkSchema.RemindDate.name, Bookmark::remindDate, query) { it.toString() }
+
+        is BookmarkRetrieveQuery.Title ->
+            DatabaseQuery(BookmarkSchema.Title.name, Bookmark::title, query)
+
+        is BookmarkRetrieveQuery.Type ->
+            DatabaseQuery(BookmarkSchema.Type.name, query) { it.toString() }
+
+        is BookmarkRetrieveQuery.Url ->
+            DatabaseQuery(BookmarkSchema.Url.name, Bookmark::url, query)
+    }
+
+    override suspend fun applyQueryToFlow(flow: Flow<Bookmark>, query: BookmarkRetrieveQuery): Flow<Bookmark> =
+        when (query) {
+            is BookmarkRetrieveQuery.CreationDate -> flow.applySelectToFlow(Bookmark::creationDate, query)
+            is BookmarkRetrieveQuery.Deadline -> flow.applySelectToFlow(Bookmark::deadline, query)
+            is BookmarkRetrieveQuery.ExpirationDate -> flow.applySelectToFlow(Bookmark::expirationDate, query)
+            is BookmarkRetrieveQuery.Favorite -> flow.filter { it.favorite == query.target }
+            is BookmarkRetrieveQuery.RemindDate -> flow.applySelectToFlow(Bookmark::remindDate, query)
+            is BookmarkRetrieveQuery.Title -> flow.applySelectToFlow(Bookmark::title, query)
+            is BookmarkRetrieveQuery.Type -> flow.filter { it.type == query.target }
+            is BookmarkRetrieveQuery.Url -> flow.applySelectToFlow(Bookmark::url, query)
+        }
+
+    override suspend fun postFetch(data: Bookmark): Bookmark {
+        // TODO get tags
+        return super.postFetch(data)
+    }
+}
